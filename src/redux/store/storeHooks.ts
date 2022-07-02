b7a6b24469f10c3e1d8edbeef6333b1038fb4ae5
Anton Arbus <anton.arbus@macbookpro>
2022-07-02 21:22:05 +0300

    some comment

--- src/redux/store/storeHooks.ts
+++ src/redux/store/storeHooks.ts
@@ -1,5 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
 
+// needed for types
 export const useSelectorTyped: TypedUseSelectorHook<RootState> = useSelector
 export const useDispatchTyped = () => useDispatch<AppDispatch>()
