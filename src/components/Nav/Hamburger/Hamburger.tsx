b900c0a841e1f1926e4a3e85c2d2ed9aa3540649
Anton Arbus <anton.arbus@macbookpro>
2022-07-08 14:45:31 +0300

    added all props to hamburger

--- src/components/Nav/Hamburger/Hamburger.tsx
+++ src/components/Nav/Hamburger/Hamburger.tsx
@@ -1,11 +1,26 @@
 import styled from 'styled-components'
 import { Fade as HamburgerIcon } from 'hamburger-react'
+import { useState } from 'react'
 // https://hamburger-react.netlify.app/
 
 export function Hamburger() {
+  const [isOpen, setOpen] = useState(false)
+  // console.log('isOpen', isOpen)
+  // setTimeout(() => { setOpen(!isOpen) }, 1000)
+
   return (
     <HamburgerContainer>
-      <HamburgerIcon size={20} color='white'/>
+      <HamburgerIcon
+        toggled={isOpen}
+        toggle={setOpen}
+        size={20}
+        color='white'
+        rounded
+        label='Show menu'
+        onToggle={toggled => {
+          if (toggled) console.log('menu opened')
+          if (!toggled) console.log('menu closed')
+        }} />
     </HamburgerContainer>
   )
 }
