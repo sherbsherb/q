4f6b043ca359d5e2089a5142dd4f9bbf8864f96e
Anton Arbus <anton.arbus@macbookpro>
2022-07-11 00:08:54 +0300

    fixed ts for calcNavMediaQueryParams func

--- src/components/Nav/calcNavMediaQueryParams.ts
+++ src/components/Nav/calcNavMediaQueryParams.ts
@@ -1,34 +1,40 @@
 import { isOverflown } from '@src/functions/isOverflown'
 
-const shrinkElementSlightly = (element) => { element.style.width = element.offsetWidth - 10 + 'px' }
+const shrinkElementSlightly = (el: HTMLElement) => { el.style.width = el.offsetWidth - 10 + 'px' }
 
-export function calcNavMediaQueryParams(nav, logo) {
+export function calcNavMediaQueryParams(nav: HTMLElement, logo: HTMLElement) {
   const gapAfterLogoWhenMediaQueryKicksIn = 50
   nav.style.width = 1000 + 'px'
 
   // get width when hide '.app' from logo
   while (!isOverflown(logo)) shrinkElementSlightly(nav)
   const screenWidthWhenHideLogoExtension = nav.offsetWidth + gapAfterLogoWhenMediaQueryKicksIn
-  nav.querySelector('.app-ext').style.display = 'none'
+  const aptExt = nav.querySelector('.app-ext')! as HTMLElement
+  aptExt.style.display = 'none'
   nav.style.width = nav.offsetWidth + 100 + 'px'
 
   // get width when hide 'uotation' from logo
   while (!isOverflown(logo)) shrinkElementSlightly(nav)
   const screenWidthWhenHideLogoPart = nav.offsetWidth + gapAfterLogoWhenMediaQueryKicksIn
-  nav.querySelector('.uotation').style.display = 'none'
+  const uotation = nav.querySelector('.uotation')! as HTMLElement
+  uotation.style.display = 'none'
   nav.style.width = nav.offsetWidth + 100 + 'px'
 
   // get width when hide icons
   while (!isOverflown(logo)) shrinkElementSlightly(nav)
   const screenWidthWhenHideIcon = nav.offsetWidth + gapAfterLogoWhenMediaQueryKicksIn
-  Array.from(nav.querySelectorAll('.icon-round-wrapper')).forEach((el) => { el.style.display = 'none' })
+  const iconWrappers = nav.querySelectorAll('.icon-round-wrapper')
+  const iconWrappersArr = Array.from(iconWrappers)
+  iconWrappersArr.forEach((el) => { (el as HTMLElement).style.display = 'none' })
   nav.style.width = nav.offsetWidth + 100 + 'px'
 
   // get width when hide text
   while (!isOverflown(logo)) shrinkElementSlightly(nav)
   const screenWidthWhenHideText = nav.offsetWidth + gapAfterLogoWhenMediaQueryKicksIn
-  Array.from(nav.querySelectorAll('.nav-item-text')).forEach((el) => { el.style.display = 'none' })
-  Array.from(nav.querySelectorAll('.icon-round-wrapper')).forEach((el) => { el.style = '' })
+  const navItems = nav.querySelectorAll('.nav-item-text')
+  const navItemsArr = Array.from(navItems)
+  navItemsArr.forEach((el) => { (el as HTMLElement).style.display = 'none' })
+  iconWrappersArr.forEach((el) => { (el as HTMLElement).style.display = '' })
   nav.style.width = nav.offsetWidth + 100 + 'px'
 
   // get width when show burger
@@ -36,11 +42,11 @@ export function calcNavMediaQueryParams(nav, logo) {
   const screenWidthWhenDisplayBurger = nav.offsetWidth + gapAfterLogoWhenMediaQueryKicksIn
 
   // show elements back on screen after calculation
-  nav.querySelector('.app-ext').style = ''
-  nav.querySelector('.uotation').style = ''
-  Array.from(nav.querySelectorAll('.icon-round-wrapper')).forEach((el) => { el.style = '' })
-  Array.from(nav.querySelectorAll('.nav-item-text')).forEach((el) => { el.style = '' })
-  nav.style = ''
+  aptExt.setAttribute('style', '')
+  uotation.setAttribute('style', '')
+  iconWrappersArr.forEach((el) => { (el as HTMLElement).setAttribute('style', '') })
+  navItemsArr.forEach((el) => { (el as HTMLElement).setAttribute('style', '') })
+  nav.setAttribute('style', '')
 
   return {
     screenWidthWhenHideLogoExtension,
