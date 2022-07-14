a167b9eb4c6ca594e8fbfc60f8e1cf302d8c5404
Anton Arbus <anton.arbus@macbookpro>
2022-07-14 12:17:54 +0300

    vite port config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 3009,
     proxy: {
       '/api': 'http://localhost:3001/'
     }
