f73ec0a33a9765dd2a14c4dff2d0331d44c479c1
Anton Arbus <anton.arbus@macbookpro>
2022-07-19 14:30:55 +0300

    shortcuts improvement

--- src/components/Nav/NavList/NavItem/Menu/MenuItem/Shortcut.tsx
+++ src/components/Nav/NavList/NavItem/Menu/MenuItem/Shortcut.tsx
@@ -6,19 +6,31 @@ type ShortcutType = {
   $isHovered: boolean
 }
 
+function capitalizeKey(keyStr: string) {
+  return keyStr
+    .toUpperCase()
+    .replaceAll('CONTROL', 'Ctrl')
+    .replaceAll('ALT', 'Alt')
+    .replaceAll('META', 'Meta')
+    .replaceAll('SHIFT', 'Shift')
+    .replaceAll('OPTION', 'Opt')
+    .replaceAll('TAB', 'Tab')
+    .replaceAll('BACKSPACE', 'Backspace')
+    .replaceAll('ENTER', 'Enter')
+    .replaceAll('DELETE', 'Delete')
+    .replaceAll('END', 'End')
+    .replaceAll('HOME', 'Home')
+    .replaceAll('PAGEDOWN', 'PageDown')
+    .replaceAll('PAGEUP', 'PageUp')
+    .replaceAll('CLEAR', 'Clear')
+}
+
 export function Shortcut({ shortcut, $isHovered }: ShortcutType) {
-  console.log(shortcut)
   const keys = shortcut.join('+').toUpperCase()
 
   return (
     <Span className='shortcut' $isHovered={$isHovered}>
-      {keys
-        .replaceAll('CONTROL', 'Ctrl')
-        .replaceAll('ALT', 'Alt')
-        .replaceAll('META', 'Meta')
-        .replaceAll('SHIFT', 'Shift')
-        .replaceAll('OPTION', 'Opt')
-      }
+      {capitalizeKey(keys)}
     </Span>
   )
 }
@@ -31,7 +43,7 @@ const Span = styled.span<PropsForSC>`
   display: ${props => props.$isHovered ? 'block' : 'none'};
   position: absolute;
   right: 10px;
-  top: -15px;
+  top: 5px;
   font-size: 10px;
   font-weight: 300;
   color: #a5a4a4;
