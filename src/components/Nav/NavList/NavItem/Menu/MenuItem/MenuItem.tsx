b0e68c62ba821b7789696b2f725cd817febe2ba9
Anton Arbus <anton.arbus@macbookpro>
2022-07-20 00:23:20 +0300

    files and folders

--- src/components/Nav/NavList/NavItem/Menu/MenuItem/MenuItem.tsx
+++ src/components/Nav/NavList/NavItem/Menu/MenuItem/MenuItem.tsx
@@ -5,12 +5,11 @@ import { MenuItemStyled } from './MenuItemStyled'
 import { TextInMenu } from './TextInMenu'
 import { RoundSpanForIconStyled } from '../../RoundSpanForIconStyled'
 import { MenuType } from '@components/Nav/navStructure'
-import { closeMenu, setMenuItemHoverIndex } from '@slices/navSlice'
-import { useDispatchTyped, useSelectorTyped as useSelector } from '@store/storeHooks'
-import { globalObject } from '@src/globalObject'
+import { setMenuItemHoverIndex } from '@slices/navSlice'
+import { useSelectorTyped as useSelector } from '@store/storeHooks'
 import { store } from '@redux/store'
-import { getMenuItemByIdsChain } from '../functions/getMenuItemByIdsChain'
 import { Shortcut } from './Shortcut'
+import { clickOnMenuItem } from './function/clickOnMenuItem'
 
 type MenuItemType = {
   menuItem: MenuType
@@ -25,45 +24,11 @@ export function MenuItem({ menuItem, hoveredMenuItemIndex }: MenuItemType) {
   const link = menuItem.link
   const shortcut = menuItem?.shortcut
 
-  type EventType = KeyboardEvent | MouseEvent | React.MouseEvent | React.KeyboardEvent
-
-  const clickOnMenuItem = (e: EventType) => {
-    const chainToClickedItem = [...store.getState().nav.idsToCurrentMenuItems, menuId]
-    const nextMenu = getMenuItemByIdsChain(chainToClickedItem)
-    const isNestedMenuAvailable = !!nextMenu.length
-    const menuItems = getMenuItemByIdsChain(store.getState().nav.idsToCurrentMenuItems)
-    const menuItem = menuItems!.find(menuItem => menuItem.id === menuId)
-    const link = menuItem?.link
-    const func = menuItem?.func
-
-    if (link) {
-      // just follow the link natively
-      store.dispatch(closeMenu())
-      return
-    }
-
-    e.preventDefault()
-
-    if (func) {
-      func()
-      store.dispatch(closeMenu())
-      return
-    }
-
-    if (isNestedMenuAvailable) {
-      globalObject.goDownInMenu && globalObject.goDownInMenu(menuId)
-    }
-  }
-
-  const hoverOverMenuItem = () => {
-    store.dispatch(setMenuItemHoverIndex(hoveredMenuItemIndex))
-  }
-
   return (
     <MenuItemStyled
       to={link || '/'}
-      onClick={clickOnMenuItem}
-      onMouseEnter={hoverOverMenuItem}
+      onClick={(e) => clickOnMenuItem(e, menuId)}
+      onMouseEnter={() => store.dispatch(setMenuItemHoverIndex(hoveredMenuItemIndex))}
       $isHovered={isHoveredState}
     >
       {isIcon && <Icon icon={menuItem.icon} />}
