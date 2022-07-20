905cc12081bcc642624f4339ffc8ed645a692c68
Anton Arbus <anton.arbus@macbookpro>
2022-07-20 18:59:53 +0300

    icons

--- src/components/Nav/navStructure.tsx
+++ src/components/Nav/navStructure.tsx
@@ -3,6 +3,8 @@ import { nanoid as id } from 'nanoid'
 import { BiUser } from 'react-icons/bi'
 import { FiSettings } from 'react-icons/fi'
 import { IoDocumentOutline, IoDocumentsOutline, IoSaveOutline, IoShareOutline } from 'react-icons/io5'
+import { IoIosSwap } from 'react-icons/io'
+
 import logo from '@components/Main/DefaultViteComponent/logo.svg'
 
 const reactIcon = React.createElement(IoShareOutline, {})
@@ -22,6 +24,92 @@ export const navStructure: MenuType[] = [
     id: 'top',
     name: 'top',
     menuItems: [
+      {
+        id: id(5),
+        icon: <IoDocumentOutline />,
+        name: 'Offer',
+        menuItems: [
+          {
+            id: id(5),
+            name: 'Previous offer',
+            icon: <IoIosSwap />
+          },
+          {
+            id: id(5),
+            name: 'Nested menu',
+            icon: <IoIosSwap />,
+            menuItems: [
+              {
+                id: id(5),
+                name: 'item in menu 1',
+                icon: '😎',
+                menuItems: [
+                  {
+                    id: id(5),
+                    name: 'long long long long long long long long name',
+                    icon: reactIcon
+                  },
+                  {
+                    id: id(5),
+                    name: 'item in menu 1',
+                    icon: null
+                  },
+                  {
+                    id: id(5),
+                    name: 'item in menu 1',
+                    icon: <img src={logo} />
+                  },
+                  {
+                    id: id(5),
+                    name: 'item in menu 1',
+                    icon: reactIcon
+                  },
+                  {
+                    id: id(5),
+                    name: 'item in menu 1',
+                    icon: reactIcon
+                  },
+                  {
+                    id: id(5),
+                    name: 'item in menu 1',
+                    icon: reactIcon
+                  },
+                  {
+                    id: id(5),
+                    name: 'item in menu 1',
+                    icon: reactIcon
+                  }
+                ]
+              },
+              {
+                id: id(5),
+                name: 'item in menu 1',
+                icon: '😎'
+              },
+              {
+                id: id(5),
+                name: 'item in menu 1',
+                icon: '😎'
+              }
+            ]
+          },
+          {
+            id: id(5),
+            name: 'item in menu 1',
+            icon: '😇'
+          },
+          {
+            id: id(5),
+            name: 'hidden menu',
+            icon: ''
+          },
+          {
+            id: id(5),
+            name: 'not hidden menu',
+            icon: '😇'
+          }
+        ]
+      },
       {
         id: id(5),
         icon: <IoSaveOutline />,
@@ -115,92 +203,6 @@ export const navStructure: MenuType[] = [
             name: 'func',
             func: () => alert('i am the function'),
             shortcut: ['control', 'c']
-          }
-        ]
-      },
-      {
-        id: id(5),
-        icon: <IoDocumentOutline />,
-        name: 'Offer',
-        menuItems: [
-          {
-            id: id(5),
-            name: 'item in menu 1',
-            icon: '😇',
-            menuItems: [
-              {
-                id: id(5),
-                name: 'item in menu 1',
-                icon: '😎',
-                menuItems: [
-                  {
-                    id: id(5),
-                    name: 'long long long long long long long long name',
-                    icon: reactIcon
-                  },
-                  {
-                    id: id(5),
-                    name: 'item in menu 1',
-                    icon: null
-                  },
-                  {
-                    id: id(5),
-                    name: 'item in menu 1',
-                    icon: <img src={logo} />
-                  },
-                  {
-                    id: id(5),
-                    name: 'item in menu 1',
-                    icon: reactIcon
-                  },
-                  {
-                    id: id(5),
-                    name: 'item in menu 1',
-                    icon: reactIcon
-                  },
-                  {
-                    id: id(5),
-                    name: 'item in menu 1',
-                    icon: reactIcon
-                  },
-                  {
-                    id: id(5),
-                    name: 'item in menu 1',
-                    icon: reactIcon
-                  }
-                ]
-              },
-              {
-                id: id(5),
-                name: 'item in menu 1',
-                icon: '😎'
-              },
-              {
-                id: id(5),
-                name: 'item in menu 1',
-                icon: '😎'
-              }
-            ]
-          },
-          {
-            id: id(5),
-            name: 'item in menu 1',
-            icon: 'IC'
-          },
-          {
-            id: id(5),
-            name: 'item in menu 1',
-            icon: '😇'
-          },
-          {
-            id: id(5),
-            name: 'hidden menu',
-            icon: ''
-          },
-          {
-            id: id(5),
-            name: 'not hidden menu',
-            icon: '😇'
           }
         ]
       },
